fbd332f6e1573f69b46753e7b8bd48b36da802c5
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-09-26 17:57:59 -0700

    Add javascript.js and add logic to create gameboard

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const gameboard = (() => {
+    const rows = 3;
+    const columns = 3;
+    const board = []; 
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push('');
+        }
+    }
+    return {board};
+})();
+
+console.log(gameboard.board);
