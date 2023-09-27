f05277a474e2d01f825c0b6777c4895a5dcbf4c8
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-09-26 18:34:14 -0700

    Add gameController module and player factory function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,34 @@
 const gameboard = (() => {
+    // Create gameboard
     const rows = 3;
     const columns = 3;
     const board = []; 
-
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
             board[i].push('');
         }
     }
-    return {board};
+    console.log(board);
 })();
 
-console.log(gameboard.board);
+
+
+
+const gameController = (() => {
+    const player = (name, marker) => {
+        return {name, marker};
+    };
+    const playerOne = player('Player One', 'X');
+    const playerTwo = player('Player Two', 'O');
+    console.log(playerOne, playerTwo);
+})();
+
+
+
+
+
+// Create gameboard
+// Add markers to gameboard
+// Create players
+// Switch turn
